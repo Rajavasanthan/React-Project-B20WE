55f5b372dcab42157eb311e664994ac7518f0135
rajavasanthan <rajavasanthan@gmail.com>
2021-04-10 11:22:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import SideBar from "./sidebar";
+import TopBar from './topbar';
+import Dashboard from './dashboard';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import User from './user';
+import Home from './home';
+import Profile from './profile';
+import ManageUsers from './manageusers';
+import CreateUser from './createuser';
+import { UserProvider } from './usercontext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="wrapper">
+        <SideBar />
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <TopBar />
+            <div class="container-fluid">
+              <Switch>
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/users" component={User} />
+                <Route path="/profile/:id" component={Profile} />
+                
+                <UserProvider>
+                  <Route path="/manage-users" component={ManageUsers} />
+                  <Route path="/create-users" component={CreateUser} />
+                </UserProvider>
+
+                <Route path="/" component={Home} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
